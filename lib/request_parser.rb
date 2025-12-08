03dbb7091e8da283bc0abf1bcd90299b6ced7fec
maedi <maediprichard@gmail.com>
2025-12-09 09:05:40 +1100

    Create Protocol::HTTP::Headers from header fields

--- lib/request_parser.rb
+++ lib/request_parser.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'io/stream'
 require 'async/http'
 require 'protocol/http'
@@ -11,10 +13,10 @@ module Low
         stream = IO::Stream(socket)
         protocol = Async::HTTP::Protocol::HTTP.default.protocol_for(stream)
 
-        request = parse_request(stream:)
-        require 'pry'; binding.pry
+        method, full_path, path, query = parse_request(stream:)
+        headers = parse_headers(stream:)
 
-        # headers = parse_headers(stream:)
+        require 'pry'; require 'pry-nav'; binding.pry
 
         # ::Protocol::HTTP::Request.new(scheme = nil, authority = nil, method = nil, path = nil, version = nil, headers = Headers.new, body = nil, protocol = nil)
       end
@@ -23,19 +25,26 @@ module Low
 
       # TODO: Handle namespaced stream type "IO:Stream".
       def parse_request(stream:)
-        request_line = stream.gets
-
-        raise StandardError, "EOF" unless request_line
-
-        method, full_path, * = request_line.strip.split(' ', 2)
+        request_line = stream.gets || raise(StandardError, 'EOF')
+        
+        method, full_path, _http_version = request_line.strip.split(' ', 3)
         path, query = full_path.split('?', 2)
-
+        
         [method, full_path, path, query]
       end
-
+      
       # TODO: Handle namespaced stream type "IO:Stream".
       def parse_headers(stream:) -> { ::Protocol::HTTP::Headers }
-        ::Protocol::HTTP::Headers.new([['key', 'value']])
+        fields = []
+
+        while (line = stream.gets.strip) do
+          break if line.strip.empty?
+
+          key, value = line.split(/:\s/, 2)
+          fields << [key, value]
+        end
+
+        ::Protocol::HTTP::Headers.new(fields)
       end
     end
   end
