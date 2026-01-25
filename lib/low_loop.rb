1c465e30d2266d87a0cfee8cba10c8df9f9fd004
maedi <maediprichard@gmail.com>
2026-01-26 09:46:18 +1100

    Namespace version inside low loop namespace

--- lib/low_loop.rb
+++ lib/low_loop.rb
@@ -10,66 +10,62 @@ require_relative 'factories/response_factory'
 require_relative 'requests/request_parser'
 require_relative 'responses/response_builder'
 
-module Low
-  class Loop
-    include Observers
+class LowLoop
+  include Observers
 
-    attr_reader :config
+  attr_reader :config
 
-    def initialize(config:, dependencies: [])
-      @config = config
+  def initialize(config:, dependencies: [])
+    @config = config
 
-      dependencies.each do |dependency|
-        observers << dependency
-      end
-
-      observers.push(self, action: :mirror) if config.mirror_mode
+    dependencies.each do |dependency|
+      observers << dependency
     end
 
-    def start
-      server = start_server
+    observers.push(self, action: :mirror) if config.mirror_mode
+  end
 
-      Fiber.set_scheduler(Async::Scheduler.new)
+  def start
+    server = start_server
 
-      Fiber.schedule do
-        loop do
-          socket = server.accept
+    Fiber.set_scheduler(Async::Scheduler.new)
 
-          Fiber.schedule do
-            request = RequestParser.parse(socket:, host: config.host, port: config.port)
-            response_event = take(event: Events::RequestEvent.new(request:))
-            response = response_event.response
+    Fiber.schedule do
+      loop do
+        socket = server.accept
 
-            ResponseBuilder.respond(config:, socket:, response:)
-          rescue StandardError => e
-            puts e.message
-          ensure
-            socket&.close
-          end
+        Fiber.schedule do
+          request = Low::RequestParser.parse(socket:, host: config.host, port: config.port)
+          response_event = take(event: Low::Events::RequestEvent.new(request:))
+          response = response_event.response
+
+          Low::ResponseBuilder.respond(config:, socket:, response:)
+        rescue StandardError => e
+          puts e.message
+        ensure
+          socket&.close
         end
       end
     end
-
-    def start_server
-      puts "Starting server @ #{config.host}:#{config.port}" unless config.matrix_mode
-
-      server = TCPServer.new(config.host, config.port)
-      server.listen(10)
-      server
-    end
-
-    # Fallback mode for when there's no dependencies and you want to know that the server is still working.
-    def mirror(event:)
-      request = event.request
-      response = Factories::ResponseFactory.html(body: "Thank you for visiting #{request.path} with body: '#{request.body}'")
-      Events::ResponseEvent.new(response:)
-    end
-
-    # Consider LowLoop a value object in the context of Observers (there can only be one).
-    def ==(other) = other.class == self.class
-    def eql?(other) = self == other
-    def hash = [self.class].hash
   end
-end
 
-LowLoop = Low::Loop
+  def start_server
+    puts "Starting server @ #{config.host}:#{config.port}" unless config.matrix_mode
+
+    server = TCPServer.new(config.host, config.port)
+    server.listen(10)
+    server
+  end
+
+  # Fallback mode for when there's no dependencies and you want to know that the server is still working.
+  def mirror(event:)
+    request = event.request
+    response = Low::Factories::ResponseFactory.html(body: "Thank you for visiting #{request.path} with body: '#{request.body}'")
+    Low::Events::ResponseEvent.new(response:)
+  end
+
+  # Consider LowLoop a value object in the context of Observers (there can only be one).
+  def ==(other) = other.class == self.class
+  def eql?(other) = self == other
+  def hash = [self.class].hash
+end
--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 module Low
-  LOOP_VERSION = '0.3.0'
+  module Loop
+    VERSION = '0.3.1'
+  end
 end
